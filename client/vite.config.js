b51b44cdb9bfba22ab7c15af18df6026c0bcd814
sadonamonday <kumaloblessing21@gmail.com>
2025-11-27 11:05:09 +0200

    fixed dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    base: '/',
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
